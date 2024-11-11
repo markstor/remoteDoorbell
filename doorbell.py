41222d44401762595eb7fc7f33c27d52c0e27239
Marc Casalprim Torres <marc.casalprim.torres@hp.com>
2024-11-11 19:50:43 +0100

    Change to component classes

--- doorbell.py
+++ doorbell.py
@@ -1,59 +1,131 @@
 import os
-from gpiozero import Button, OutputDevice
+import json
+import time
+from gpiozero import Button, OutputDevice, DigitalOutputDevice
 from signal import pause
 import paho.mqtt.client as mqtt
 
-# GPIO pins for microphone and speaker control
-mic_control_pin = 23  # GPIO to control microphone (or ADC power)
-speaker_control_pin = 24  # GPIO to control speaker (or DAC/amplifier power)
-
-# Setup GPIO
-mic_control = OutputDevice(mic_control_pin)
-speaker_control = OutputDevice(speaker_control_pin)
-
 # MQTT Setup
-MQTT_BROKER = "your_mqtt_broker_ip"
-MQTT_TOPIC_MIC = "home/doorbell/request_mic"
-MQTT_TOPIC_SPEAKER = "home/doorbell/request_speaker"
+MQTT_BROKER = "ha.casalprim.xyz"
+
+
+MQTT_DISCOVERY_PREFIX = "homeassistant"
+MQTT_UNIQUE_ID = "doorbell1992"
+
+class ButtonController:
+    def __init__(self, gpio_pin, button_name=None, active_time=0.2):
+        self.active_time = active_time
+        self.gpio_pin = gpio_pin
+        self.button_name = button_name
+        if self.button_name is None:
+            self.button_name = f"Button{self.gpio_pin}"
+        self.configure_input_button()
+
+    def configure_input_button(self):
+        self.input_button = Button(self.gpio_pin, pull_up=False)
+        self.input_button.when_pressed = self.on_button_press
+
+    def on_button_press(self):
+        print(f"Button on pin {self.input_button.pin.number} was pressed!")
+        
+        # Log button press via MQTT
+        message = {"button": self.input_button.pin.number, "action": "pressed"}
+        client.publish(self.topic, json.dumps(message), qos=1)
+
+    def activate_button(self):
+        print(f"Activating button on pin {self.output_control.pin.number}")
+        
+        self.input_button.close()
+        with DigitalOutputDevice(self.gpio_pin, active_high=False) as output_control:
+            output_control.on()
+            time.sleep(self.active_time)
+            output_control.off()
+        self.configure_input_button()
+
+    @property
+    def object_id(self):
+        # sanitize button name
+        return f"{self.button_name.lower().replace(' ', '_')}"
+
+    @property
+    def topic(self):
+        topic = f"home/doorbell/{self.object_id}"
+        return topic
+
+    def component_discovery_payload(self):
+        return {
+            "name": self.button_name,
+            "state_topic": f"{self.topic}/state",
+            "availability_topic": f"{self.topic}/availability",
+            "device_class": "motion",  # Adjust as necessary
+            "payload_on": "pressed",
+            "payload_off": "released",
+            "unique_id": f"{MQTT_UNIQUE_ID}_{self.object_id}",
+            "device": {"identifiers": [MQTT_UNIQUE_ID]}  # Link to main device
+        }
+    
+    
+
+def discovery_topic():
+    prefix = MQTT_DISCOVERY_PREFIX
+    component = "device"
+    object_id = MQTT_UNIQUE_ID
+    topic = f"{prefix}/{component}/{object_id}"
+    return topic
+
+def generate_discovery_payload(components_dict):
+    discovery_payload = {
+    "dev": {
+        "ids": MQTT_UNIQUE_ID,  # Unique ID for the device
+        "name": "Interfono",
+        "mf": "PRIM, S.A.",
+        "mdl": "UltraGuard",
+        "sw": "1.0",
+        "sn": "1234567890",
+        "hw": "v1"
+    },
+    "o": {
+        "name": "PRIM System",
+        "sw": "0.1",
+        "url": "https://blog.casalprim.xyz"
+    },
+    "cmps": components_dict,
+    "state_topic": "home/doorbell/state",
+    "qos": 1
+    }
+    return discovery_payload
+
+def publish_discovery_payload(buttons):
+    components = [button.component_discovery_payload() for button in buttons]
+    discovery_payload = generate_discovery_payload(components)
+    prefix = MQTT_DISCOVERY_PREFIX
+    component = "device"
+    object_id = MQTT_UNIQUE_ID
+    topic = f"{prefix}/{component}/{object_id}"
+    client.publish(topic, json.dumps(discovery_payload), qos=1, retain=True)
+
+buttons = [
+    ButtonController(17, "doorButton"),
+    ButtonController(27, "videoButton")
+]
 
 def on_connect(client, userdata, flags, rc):
-    print(f"Connected to MQTT broker with result code {rc}")
-    client.subscribe([(MQTT_TOPIC_MIC, 0), (MQTT_TOPIC_SPEAKER, 0)])
+    print(f"Connected with result code {rc}")
+    # Subscribe to all button topics
+    for button in buttons:
+        client.subscribe(button.topic)
+    # Publish discovery payload
+    publish_discovery_payload(buttons)
+    print("Discovery payload published")
 
 def on_message(client, userdata, msg):
-    if msg.topic == MQTT_TOPIC_MIC:
-        if msg.payload.decode() == 'on':
-            activate_microphone()
-        elif msg.payload.decode() == 'off':
-            deactivate_microphone()
-    elif msg.topic == MQTT_TOPIC_SPEAKER:
-        if msg.payload.decode() == 'on':
-            activate_speaker()
-        elif msg.payload.decode() == 'off':
-            deactivate_speaker()
+    print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
+    for button in buttons:
+        if msg.topic == button.topic:
+            button.activate_button()
 
-# Functions to control the microphone and speaker
-def activate_microphone():
-    print("Activating microphone...")
-    mic_control.on()  # Power on microphone/ADC
-    # Setup go2rtc to stream audio from microphone
-    os.system("go2rtc -config mic_stream.yaml &")
 
-def deactivate_microphone():
-    print("Deactivating microphone...")
-    mic_control.off()  # Power off microphone/ADC
-    os.system("pkill go2rtc")
 
-def activate_speaker():
-    print("Activating speaker...")
-    speaker_control.on()  # Power on speaker/DAC
-    # Setup go2rtc to stream audio to speaker
-    os.system("go2rtc -config speaker_stream.yaml &")
-
-def deactivate_speaker():
-    print("Deactivating speaker...")
-    speaker_control.off()  # Power off speaker/DAC
-    os.system("pkill go2rtc")
 
 # Setup MQTT client
 client = mqtt.Client()
@@ -69,8 +141,7 @@ try:
     pause()
 
 except KeyboardInterrupt:
-    deactivate_microphone()
-    deactivate_speaker()
+    del buttons
     print("Exiting script...")
 
 client.loop_stop()
