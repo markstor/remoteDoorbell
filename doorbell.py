72aeaeea6b04a86df210ea1b2f5b563885f87b9c
Marc Casalprim Torres <marc.casalprim.torres@hp.com>
2024-11-12 10:20:22 +0100

    Best practices

--- doorbell.py
+++ doorbell.py
@@ -1,16 +1,22 @@
-import os
+import logging
 import json
 import time
 from gpiozero import Button, OutputDevice, DigitalOutputDevice
 from signal import pause
 import paho.mqtt.client as mqtt
 
-# MQTT Setup
-MQTT_BROKER = "homeassistant.casa.casalprim.xyz"
+logger = logging.getLogger(__name__)
 
+# Load MQTT setup from file
+with open('mqtt_config.json') as f:
+    credentials = json.load(f)
+    MQTT_HOST = credentials['host']
+    MQTT_PORT = credentials['port']
+    MQTT_USERNAME = credentials['username']
+    MQTT_PASSWORD = credentials['password']
 
-MQTT_DISCOVERY_PREFIX = "homeassistant"
-MQTT_UNIQUE_ID = "doorbell1992"
+HA_DISCOVERY_PREFIX = "homeassistant"
+DEVICE_UNIQUE_ID = "doorbell1234"
 
 class ButtonController:
     def __init__(self, gpio_pin, button_name=None, active_time=0.2):
@@ -26,14 +32,14 @@ class ButtonController:
         self.input_button.when_pressed = self.on_button_press
 
     def on_button_press(self):
-        print(f"Button on pin {self.input_button.pin.number} was pressed!")
+        logging.info(f"Button on pin {self.input_button.pin.number} was pressed!")
         
         # Log button press via MQTT
         message = {"button": self.input_button.pin.number, "action": "pressed"}
         client.publish(self.topic, json.dumps(message), qos=1)
 
     def activate_button(self):
-        print(f"Activating button on pin {self.output_control.pin.number}")
+        logging.info(f"Activating button on pin {self.output_control.pin.number}")
         
         self.input_button.close()
         with DigitalOutputDevice(self.gpio_pin, active_high=False) as output_control:
@@ -58,22 +64,22 @@ class ButtonController:
             "state_topic": f"{self.topic}/state",
             "availability_topic": f"{self.topic}/availability",
             "device_class": "motion",
-            "unique_id": f"{MQTT_UNIQUE_ID}_{self.object_id}",
+            "unique_id": f"{DEVICE_UNIQUE_ID}_{self.object_id}",
         }}
     
     
 
 def discovery_topic():
-    prefix = MQTT_DISCOVERY_PREFIX
+    prefix = HA_DISCOVERY_PREFIX
     component = "device"
-    object_id = MQTT_UNIQUE_ID
+    object_id = DEVICE_UNIQUE_ID
     topic = f"{prefix}/{component}/{object_id}"
     return topic
 
 def generate_discovery_payload(components_dict):
     discovery_payload = {
     "dev": {
-        "ids": MQTT_UNIQUE_ID,  # Unique ID for the device
+        "ids": DEVICE_UNIQUE_ID,  # Unique ID for the device
         "name": "Interfono",
         "mf": "PRIM, S.A.",
         "mdl": "UltraGuard",
@@ -92,56 +98,61 @@ def generate_discovery_payload(components_dict):
     }
     return discovery_payload
 
-def publish_discovery_payload(buttons):
+def publish_discovery_payload(client, buttons):
     components = {}
     for button in buttons:
         components.update(button.component_discovery_payload())
     discovery_payload = generate_discovery_payload(components)
-    prefix = MQTT_DISCOVERY_PREFIX
+    prefix = HA_DISCOVERY_PREFIX
     component = "device"
-    object_id = MQTT_UNIQUE_ID
-    topic = f"{prefix}/{component}/{object_id}"
+    object_id = DEVICE_UNIQUE_ID
+    topic = f"{prefix}/{component}/{object_id}/config"
     client.publish(topic, json.dumps(discovery_payload), qos=1, retain=True)
 
-buttons = [
-    ButtonController(14, "doorButton"),
-    ButtonController(15, "videoButton")
-]
+def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    buttons  = [
+        ButtonController(14, "doorButton"),
+        ButtonController(15, "videoButton")
+    ]
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    # Subscribe to all button topics
-    for button in buttons:
-        client.subscribe(button.topic)
-    # Publish discovery payload
-    publish_discovery_payload(buttons)
-    print("Discovery payload published")
+    def on_connect(client, userdata, flags, rc):
+        logging.info(f"Connected with result code {rc}")
+        # Subscribe to all button topics
+        for button in buttons:
+            client.subscribe(button.topic)
+        # Publish discovery payload
+        publish_discovery_payload(client, buttons)
+        logging.info("Discovery payload published")
 
-def on_message(client, userdata, msg):
-    print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
-    for button in buttons:
-        if msg.topic == button.topic:
-            #button.activate_button()
-            print(button.name, "detected")
+    def on_message(client, userdata, msg):
+        logging.info(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
+        for button in buttons:
+            if msg.topic == button.topic:
+                #button.activate_button()
+                logging.info(f"Button {button.object_id} activated")
 
-# Setup MQTT client
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set("your_username", "your_password")
+    # Setup MQTT client
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-client.connect(MQTT_BROKER, 1883, 60)
+    client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    client.connect(MQTT_HOST, MQTT_PORT, 60)
 
-# Start MQTT loop in the background
-client.loop_start()
+    # Start MQTT loop in the background
+    client.loop_start()
 
-try:
-    # Wait for events indefinitely
-    pause()
+    try:
+        # Wait for events indefinitely
+        pause()
 
-except KeyboardInterrupt:
-    del buttons
-    print("Exiting script...")
+    except KeyboardInterrupt:
+        del buttons
+        logging.info("Exiting script...")
 
-client.loop_stop()
-client.disconnect()
+    client.loop_stop()
+    client.disconnect()
+
+if __name__ == "__main__":
+    main()
